d587e7af6767446f974e1a1bd98effea698a0f44
tong <tong@disktree.net>
2014-09-27 02:02:16 +0200

    ...

--- src/om/util/UserAgentUtil.hx
+++ src/om/util/UserAgentUtil.hx
@@ -5,5 +5,9 @@ class UserAgentUtil {
 	public static inline function detectWebgl() : Bool {
 		return untyped __js__("(function(){try{return !!window.WebGLRenderingContext&&!!document.createElement('canvas').getContext('experimental-webgl');}catch(e){return false;}})()" );
 	}
+
+	public static function detectMobile() : Bool {
+		return untyped __js__('/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)') );
+	}
 	
 }
