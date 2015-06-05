8bab34f16f4b837523a4451598eb70c32184ae23
tong <tong@disktree.net>
2015-06-06 01:01:55 +0200

    Nodejs: Time.now support

--- src/om/Time.hx
+++ src/om/Time.hx
@@ -4,7 +4,11 @@ class Time {
 
 	public static inline function now() : Float {
 
-		#if js
+		#if nodejs
+		var hr = js.Node.process.hrtime();
+		return ( hr[0] * 1e9 + hr[1] )/ 1e6;
+
+		#elseif js
 		return untyped window.performance.now();
 
 		#elseif sys
