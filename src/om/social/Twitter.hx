7ed206a662f73e5ce3493dff9ddf78a3d3411980
tong <tong@disktree.net>
2015-06-12 10:41:13 +0200

    Fix

--- src/om/social/Twitter.hx
+++ src/om/social/Twitter.hx
@@ -6,9 +6,9 @@ class Twitter {
 
     public static function createTweetURL( ?text : String, ?url : String, ?via : String ) : String {
         var str = 'https://twitter.com/intent/tweet?';
-        if( text != null ) str += 'text=$text';
-        if( url != null ) str += 'url=$url';
-        if( via != null ) str += 'via=$via';
+        if( text != null ) str += '&text=$text';
+        if( url != null ) str += '&url=$url';
+        if( via != null ) str += '&via=$via';
         return str;
     }
 
