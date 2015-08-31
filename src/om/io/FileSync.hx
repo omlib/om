0664f52dfea4be209eb13f86ed90e12dde97ccfa
Tong <tong@disktree.net>
2015-08-31 14:12:17 +0200

    ...

--- src/om/io/FileSync.hx
+++ src/om/io/FileSync.hx
@@ -1,18 +1,21 @@
 package om.io;
 
+#if sys
+
 import sys.FileSystem;
 import sys.FileSystem.exists;
 import sys.FileSystem.isDirectory;
 import sys.FileSystem.createDirectory;
 import sys.FileSystem.readDirectory;
 import sys.io.File;
+import sys.io.File.copy;
 
 using om.io.FileUtil;
 
 class FileSync {
 
 	/**
-		Returns true if target file does not exist or is older as source file.
+		Returns true if target file doesn't exist or is older as source file.
 	*/
 	public static function needsUpdate( source : String, target : String ) : Bool {
 		return !exists( target ) ? true : source.modTime() > target.modTime();
@@ -24,7 +27,7 @@ class FileSync {
 		if( !exists( source ) || isDirectory( source ) )
 			return false;
 		if( needsUpdate( source, target ) ) {
-			File.copy( source, target );
+			copy( source, target );
 			return true;
 		}
 		return false;
@@ -34,24 +37,25 @@ class FileSync {
 	*/
 	public static function syncDirectory( source : String, target : String, recursive = true ) : Bool {
 		if( !exists( source ) )
-	//		throw 'Source directory not found ($source)';
-			return false;
+			throw 'Source directory not found ($source)';
 		if( !exists( target ) )
 			createDirectory( target );
 		for( f in readDirectory( source ) ) {
 			var sp = '$source/$f';
 			var tp = '$target/$f';
 			if( isDirectory( sp ) ) {
-				if( recursive )
-					syncDirectory( sp, tp );
+				//if( !exists( target ) ) createDirectory( target );
+				if( recursive ) syncDirectory( sp, tp );
 			} else {
 				if( exists( tp ) ) {
-					if( needsUpdate( sp, tp ) ) File.copy( sp, tp );
+					if( needsUpdate( sp, tp ) ) copy( sp, tp );
 				} else {
-					File.copy( sp, tp );
+					copy( sp, tp );
 				}
 			}
 		}
 		return true;
 	}
 }
+
+#end
